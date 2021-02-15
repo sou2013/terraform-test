5160d8c26bde97eb67c2fdd723a2f69a4de478ca
Charles Lee <charles.lee@sktana.com>
2021-02-14 19:11:37 -0800

    test

--- first-boot-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "hello world"
-
-apt update
-apt install mariadb-server -y
-systemctl start mysql
-systemctl enable mysql
--- first-boot-http.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "hello world"
-
-apt update
-apt install apache2 -y
-systemctl start apache2
-systemctl enable apache2
