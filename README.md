08edc52371e88c1efa2b97897d4305f9b2b2ff8c
Charles Lee <charles.lee@sktana.com>
2021-02-14 17:28:19 -0800

    Add lines in README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # terraform-test
-PLease use your own profile`
+PLease use your aws own profile`
+
+create ssh key
+ssh-keygen -t rsa -m PEM 
+you can copy id_rsa to anyname.pem
+then
+chmod 400 anyname.pem
+you will use "ssh -i anyname.pem ubuntu@ec2dnsname
+
+then create
+public key file
+ssh-keygen -y -f /path_to_key_pair/my-key-pair.pem >> terraform_ec2_key.pub
--- ssh-key.tf
+++ ssh-key.tf
@@ -3,5 +3,5 @@
 # Create default ssh publique key
 resource "aws_key_pair" "user_key" {
   key_name   = "charles1"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNjUjnmfXIsDQHPGS6y45a/cxVVOA6SFnA2nZZW5WFEwMj6RSBK+/oB+Rrcu+UDKQs9bqz1Nte7BM+N2Cvf295z7FKKtn8MBGG4NXXqq3wi9FwCinJf6oNrEqtoJnJNJwt9zyGvqxqY/yW2eG9JAmFO1YZsNB7CD3+pX7DB26ZvNuufWthUzxxnWbyKlW7nnAKtpy+Qw9YGx3xpkOPQ4AS0UJqFjM1mXv9zeVzWVOGt3nEbihjHwRUuiWM2EkyEsPEm0nscPLxoGKbrKolnrDwy/p9ZdFCTXeSrYwAlWuE0BAzBDBeR7ErApQPJ3vvdYwf49BFwEKg/dC1lP0fn8ch charles.lee@aktana.com"
+  public_key = "file(terraform_ec2_key.pub)"
 }
